66305efca87cf6e98448506fdff4b6e5cede8f5b
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-09-11 14:51:19 -0400

    commit tree 3fddd71eccca0243f1676e709bc9320555790661

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function superbowlWin(array) {
+    const result1 = array.find( function(r) {
+        return r.result === "W" })
+        if (result1) {
+            return result1.year
+        } else {
+            return undefined
+        }
 
+} 
+
+
+    // get debugger going to find out what the array is.
+    // figure out how to return an the objects year when winner returns true.
